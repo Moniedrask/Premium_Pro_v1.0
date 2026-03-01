b3ff486b6f627dde16c9b79c8b0a1b46d7290e10
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-02-28 21:23:28 -0500

    Add files via upload

---
+++ android/app/src/main/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.premiumpro.editor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // MainActivity para Android Embedding V2
+    // No requiere código adicional para configuración básica
+}
