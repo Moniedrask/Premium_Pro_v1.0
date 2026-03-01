f3d3862a7a1f247b9adf8c65f780c6ef8f9a8ca1
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-02-28 23:41:47 -0500

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/premiumpro/editor/MainActivity.kt
+++ android/app/src/main/kotlin/com/premiumpro/editor/MainActivity.kt
@@ -3,6 +3,4 @@ package com.premiumpro.editor
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // MainActivity para Android Embedding V2
-    // Configuración mínima requerida para Flutter
 }
