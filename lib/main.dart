77801d5df6f2da5e1be2c2dbefd76c2ffff85cfe
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-03-01 13:58:52 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
 import 'services/media_processor.dart';
 import 'services/ai_manager.dart';
 import 'widgets/timeline_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  FFmpegKitConfig.enableLogs(true);
   runApp(const PremiumProApp());
 }
 
