50538aff9651e0e8a37b9ac5ef931847e092b940
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-02-28 22:27:49 -0500

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/premiumpro/editor/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.premiumpro.editor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // MainActivity para Android Embedding V2
+    // Configuración mínima requerida para Flutter
+}
