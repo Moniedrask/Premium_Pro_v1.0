1096e1a511fc894c5e44fcee28e3eff2628264c0
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-03-01 21:50:06 -0500

    Actualizar media_processor.dart

--- lib/services/media_processor.dart
+++ lib/services/media_processor.dart
@@ -1,4 +1,5 @@
-import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
+import 
+'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 import 'package:ffmpeg_kit_flutter_new/return_code.dart';
 import 'package:flutter/foundation.dart';
 
