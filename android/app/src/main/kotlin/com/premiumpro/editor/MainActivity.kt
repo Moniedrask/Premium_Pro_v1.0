dd064cd18949d8bd0e944f50954b195ecc83e928
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-03-03 02:22:26 -0500

    Actualizar MainActivity.kt

--- android/app/src/main/kotlin/com/premiumpro/editor/MainActivity.kt
+++ android/app/src/main/kotlin/com/premiumpro/editor/MainActivity.kt
@@ -3,6 +3,5 @@ package com.premiumpro.editor
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // MainActivity para Android Embedding V2
-    // Compatible con AGP 8.7 y Flutter 3.41.0
+    // Compatible con Android Embedding V2
 }
