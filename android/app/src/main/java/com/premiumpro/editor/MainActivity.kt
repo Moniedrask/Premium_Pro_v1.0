a7ac3d8037ea0cb84b2e8c4d0279b0df43d67518
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-03-04 02:27:16 -0500

    Eliminar MainActivity.kt

--- android/app/src/main/java/com/premiumpro/editor/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.premiumpro.editor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // MainActivity para Android Embedding V2
-    // No requiere código adicional para configuración básica
-}
