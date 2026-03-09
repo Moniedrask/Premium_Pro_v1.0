c0aa8ef073fe697719563e7630cf565399504b02
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-03-09 16:12:38 -0500

    Actualizar timeline_widget.dart

--- lib/widgets/timeline_widget.dart
+++ lib/widgets/timeline_widget.dart
@@ -30,9 +30,9 @@ class TimelineWidgetState extends State<TimelineWidget> {
     {'name': '240p', 'width': 426, 'height': 240},
     {'name': '360p', 'width': 640, 'height': 360},
     {'name': '480p', 'width': 854, 'height': 480},
-    {'name': '720p', 'width': 1280, 'height': 720},
-    {'name': '1080p', 'width': 1920, 'height': 1080},
-    {'name': '1440p', 'width': 2560, 'height': 1440},
+    {'name': '720p(1.25K)', 'width': 1280, 'height': 720},
+    {'name': '1080p(2K)', 'width': 1920, 'height': 1080},
+    {'name': '1440p(2.5K)', 'width': 2560, 'height': 1440},
     {'name': '4K', 'width': 3840, 'height': 2160},
     {'name': '8K', 'width': 7680, 'height': 4320},
   ];
