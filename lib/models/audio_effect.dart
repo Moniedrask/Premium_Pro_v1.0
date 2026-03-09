987921be99df2beec2658c2849a06b5152408d9d
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-03-09 14:55:18 -0500

    Crear lib/models/audio_effect.dart

---
+++ lib/models/audio_effect.dart
@@ -0,0 +1,6 @@
+// Placeholder para efectos de audio (por implementar)
+class AudioEffect {
+  // Por ahora vacío, pero necesario para evitar errores de compilación
+  Map<String, dynamic> toJson() => {};
+  factory AudioEffect.fromJson(Map<String, dynamic> json) => AudioEffect();
+}
