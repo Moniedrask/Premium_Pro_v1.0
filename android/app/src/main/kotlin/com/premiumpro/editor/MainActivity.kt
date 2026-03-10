d4618a32ef5402f3f2050e6aab223177fdbe48e8
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-03-10 13:15:30 -0500

    Actualizar MainActivity.kt

--- android/app/src/main/kotlin/com/premiumpro/editor/MainActivity.kt
+++ android/app/src/main/kotlin/com/premiumpro/editor/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.premiumpro.editor
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import com.arthenica.ffmpegkit.flutter.FFmpegKitFlutterPlugin
 
 class MainActivity: FlutterActivity() {
-    // Compatible con Android Embedding V2
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // Registro manual del plugin FFmpegKit
+        FFmpegKitFlutterPlugin(flutterEngine.dartExecutor.binaryMessenger)
+    }
 }
