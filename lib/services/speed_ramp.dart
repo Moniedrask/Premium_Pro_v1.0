55ce1020b3fbfc2ea337c5d2198817db96d2a154
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-03-11 14:56:46 -0500

    Actualizar speed_ramp.dart

--- lib/services/speed_ramp.dart
+++ lib/services/speed_ramp.dart
@@ -1,32 +1,4 @@
-import 'package:flutter/material.dart';
-
-class SpeedSegment {
-  final Duration start;
-  final Duration end;
-  final double speed; // 0.1 a 16.0
-  final Color color;
-
-  SpeedSegment({
-    required this.start,
-    required this.end,
-    this.speed = 1.0,
-    this.color = Colors.blueAccent,
-  });
-
-  Map<String, dynamic> toJson() => {
-        'start': start.inMilliseconds,
-        'end': end.inMilliseconds,
-        'speed': speed,
-      };
-
-  factory SpeedSegment.fromJson(Map<String, dynamic> json) {
-    return SpeedSegment(
-      start: Duration(milliseconds: json['start']),
-      end: Duration(milliseconds: json['end']),
-      speed: json['speed'] ?? 1.0,
-    );
-  }
-}
+import '../models/speed_segment.dart';
 
 class SpeedRampService {
   /// Genera el filtro setpts para FFmpeg a partir de una lista de segmentos
