c57150197cdaeb66a6f4cb2c349b8f3d8fcbd6d4
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-03-10 20:39:56 -0500

    Actualizar MainActivity.kt

--- android/app/src/main/kotlin/com/premiumpro/editor/MainActivity.kt
+++ android/app/src/main/kotlin/com/premiumpro/editor/MainActivity.kt
@@ -2,6 +2,4 @@ package com.premiumpro.editor
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    // Configuración estándar - el plugin se registra automáticamente
-}
+class MainActivity: FlutterActivity()
